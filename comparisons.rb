a79744021adf2260a2b694136880d2ada94d98d6
Sam Puttman <daputter2@gmail.com>
2023-08-29 13:29:54 -0500

    add code and answers

--- comparisons.rb
+++ comparisons.rb
@@ -17,29 +17,36 @@ number_students = 20
 string_teachers = "4"
 numberDogs = 0
 
-# EXAMPLE: print the result of the comparison: is number_seachers greater than number_students?
+# EXAMPLE: print the result of the comparison: is number_teachers greater than number_students?
 puts "Is number_teachers greater than number_students?", number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "Is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to string_teachers?", number_teachers == string_teachers    
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to number_students", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "Is number_students greaater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "Is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "Is number of students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "Is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -54,20 +61,28 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+#compares if Integer 4 is less than Integer 
+# --> true
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+#compares if Integer 4 is less than value of assignment operator "books" 
+# --> false
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+#compares if value of assignment operator "friends" is greater than value of assignment operator "siblings" 
+# --> true
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+#compares if value of assignment operator "attendees" is not equal to value of assignment operator "meals" 
+# --> true
 
 
 #-------------------
@@ -90,15 +105,25 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts "Dog loves to play and loves treats!", loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts "Dog loves to play and loves the dog park!", loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts "Dogs loves to play or loves the dog park", loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+if loves_to_play == true && age < 2
+    puts "The puppy loves to play!"
+elsif loves_to_play == true && age > 2
+    puts "The dog loves to play!"
+elsif loves_to_play == false && age < 2
+    puts "The puppy does not love to play. :("
+elsif loves_to_play == false && age > 2
+    puts "The dog does not love to play. :("
+else 
+    puts "How the heck did you get here?"
+end
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: It evaluated to "The puppy loves to play!"
